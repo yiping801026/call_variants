8a5916ecc71fa4c4097298e54c74ae7390f62141
yiping801026 <62942998+yiping801026@users.noreply.github.com>
2022-09-05 15:54:07 +0800

    Update call_var_single.sh
    change comments

--- call_var_single.sh
+++ call_var_single.sh
@@ -12,62 +12,62 @@ do
     filename=$(echo $file | cut -d . -f1)
     sample_name=$(echo $filename | cut -d _ -f 1-3)
     echo $sample_name
-    #1.bam文件,添加group：
+    #1. add group to bam files：
     picard AddOrReplaceReadGroups I=$file O=$dir_path/output/"${sample_name}_group.bam" RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=$sample_name
-    #2.bam文件，添加.bai文件：
+    #2. add.bai files for bam files：
     cd $dir_path/output/
     samtools index $dir_path/output/"${sample_name}_group.bam" 
-    #3. 生成中间文件gvcf
+    #3. get the middle files .gvcf
     $gatk HaplotypeCaller --emit-ref-confidence GVCF --sample-name $sample_name -R $ref -I $dir_path/output/"${sample_name}_group.bam" -L $bed -O $dir_path/output/"${sample_name}.g.vcf" && echo "** gvcf done **"
-    #4. 通过gvcf检测变异
+    #4. filter for gvcf
     $gatk GenotypeGVCFs \
     -R $ref \
     -V $dir_path/output/"${sample_name}.g.vcf"  \
     -L $bed \
     -O $dir_path/output/"${sample_name}.vcf"  && echo "** vcf done **"
-    #5. 压缩 
+    #5. zip 
     bgzip -f $dir_path/output/"${sample_name}.vcf"
     #6.构建tabix索引
     tabix -p vcf $dir_path/output/"${sample_name}.vcf.gz"
-    #7. 使用SelectVariants，选出SNP
+    #7. use SelectVariants to select SNP
     $gatk SelectVariants \
     -select-type SNP \
     -V $dir_path/output/"${sample_name}.vcf.gz" \
     -O $dir_path/output/"${sample_name}.snp.vcf.gz"
 
-    #8. 为SNP作硬过滤
+    #8. hard filter for SNPs
     $gatk VariantFiltration \
     -V $dir_path/output/"${sample_name}.snp.vcf.gz" \
     --filter-expression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 3.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
     --filter-name "Filter" \
     -O $dir_path/output/"${sample_name}.snp.filter.vcf.gz"
 
-    #9. 使用SelectVariants，选出Indel
+    #9. use SelectVariants to choose Indel
     $gatk SelectVariants \
     -select-type INDEL \
     -V $dir_path/output/"${sample_name}.vcf.gz" \
     -O $dir_path/output/"${sample_name}.indel.vcf.gz"
 
-    #10. 为Indel作过滤
+    #10. hard filter for Indel
     $gatk VariantFiltration \
     -V $dir_path/output/"${sample_name}.indel.vcf.gz" \
     --filter-expression "QD < 2.0 || FS > 200.0 || SOR > 10.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
     --filter-name "Filter" \
     -O $dir_path/output/"${sample_name}.indel.filter.vcf.gz"
 
-    #11. 重新合并过滤后的SNP和Indel
+    #11. bind SNP and Indel
     $gatk  MergeVcfs \
     -I $dir_path/output/"${sample_name}.snp.filter.vcf.gz" \
     -I $dir_path/output/"${sample_name}.indel.filter.vcf.gz" \
     -O $dir_path/output/"${sample_name}.filter.vcf.gz"
 
-    #12. 删除中间无用文件
+    #12. remove useless files 
     rm -f $dir_path/output/"${sample_name}.snp.filter.vcf.gz" \
     $dir_path/output/"${sample_name}.indel.filter.vcf.gz" \
     $dir_path/output/"${sample_name}.snp.vcf.gz" \
     $dir_path/output/"${sample_name}.indel.vcf.gz"
 
-    #13. 注释
+    #13. annotiation for vcf 
     cd annovar_dir
     perl table_annovar.pl $dir_path/output/"${sample_name}.filter.vcf" humandb/ -buildver hg38 -out "${sample_name}_final" -remove -protocol refGene,avsnp150,gnomad_genome,exac03,1000g2015aug_eas,1000g2015aug_all,clinvar_20170501,cosmic70 -operation g,f,f,f,f,f,f,f -nastring . -vcfinput -polish
 
